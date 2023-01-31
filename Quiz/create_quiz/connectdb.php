5e3e7b89897bb0ab521221351cf0fd953bf8d924
mai <117514909+Kyoyx123@users.noreply.github.com>
2023-01-31 09:43:49 +0700

    sadf

---
+++ Quiz/create_quiz/connectdb.php
@@ -0,0 +1,17 @@
+<?php
+    // $server = 'localhost';
+    // $username = 'ctnphrae_exam';
+    // $password = 'ctnphrae_exam';
+    // $dbname = 'ctnphrae_exam';
+
+    $server = 'localhost';
+    $username = 'root';
+    $password = 'root1234';
+    $dbname = 'ctn_qui';
+
+    $conn = new mysqli($server,$username,$password,$dbname);
+
+    if($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
